2fdc4d4c05376ee4578e727b3376c23f4a8d1362
Egor <hikeh67@gmail.com>
2026-09-03 21:34:15 +0300

    Update db

--- app/db/session.py
+++ app/db/session.py
@@ -6,3 +6,9 @@ from app.core.config import settings
 
 engine = create_async_engine(settings.database_url, pool_pre_ping=True)
 SessionFactory = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_db() -> AsyncIterator[AsyncSession]:
+
+    async with SessionFactory() as session:
+        yield session
